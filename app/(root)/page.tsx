fa2035aeaf1343a5c1648976bf113403ea4851c9
Sid-Bluemax <sid@bluemaxpartners.com>
2025-02-14 23:40:52 -0600

    [ADD] snaprevive

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      {/* <UserButton /> */}
+    </div>
+  )
+}
+
+export default Home;
